29f89162b89f1ce4dd886ff80fe3234e69952fa0
adilarya <134574963+adilarya@users.noreply.github.com>
2025-03-20 19:49:51 -0500

    Added Documentation for auth.ts, coords.ts

--- src/coords.ts
+++ src/coords.ts
@@ -5,6 +5,11 @@ for (let i = 1; i <= 60; ++i) {
     proj4.defs('utm' + i + 's', `+proj=utm +zone=${i} +south +datum=WGS84 +units=m +no_defs +type=crs`);
 }
 
+/**Determines the utm zone a given coordinate is in
+ * @param longitude the longitude in degrees
+ * @param latitude the latitude in degrees
+ * @returns the name of the utm zone
+ */
 // Ported from https://github.com/Turbo87/utm/blob/master/utm/conversion.py
 export function getUtmZone(longitude: number, latitude: number): string {
     // Normalize longitude to be in the range [-180, 180)
@@ -34,10 +39,21 @@ export function getUtmZone(longitude: number, latitude: number): string {
     return 'utm' + zone + (latitude > 0 ? "n" : "s");
 }
 
+/** Takes utm coords that are xy and zones and transforms them to longitude and latitude.
+ * 'EPSG:4326' is a standardized numbering system for coordinate systems.
+ * @param utm_coords number array of [x,y] or more arguments
+ * @param utm_zone the central area of the zone
+ * @returns the longitude and latitute in a number array
+ */
 export function utmToLongLat(utm_coords: number[], utm_zone: string): number[] {
     return proj4(utm_zone, 'EPSG:4326', utm_coords);
 }
 
+/**Takes longitude and latitude coords and transforms them to xy.
+ * @param long_lat number array of longitude and latitude degrees
+ * @param utm_zone name of the central area of the zone
+ * @returns the xy coordinates in a number array
+ */
 export function longLatToUtm(long_lat: number[], utm_zone: string): number[] {
     return proj4('EPSG:4326', utm_zone, long_lat);
 }
